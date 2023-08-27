2240274edbad96196ae86ea37133247f3c7fe51c
Tarxos <ftarxos@gmail.com>
2023-08-27 13:50:21 +0300

    add contractors management page

--- Application/Services/ContractorsRepository.cs
+++ Application/Services/ContractorsRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Planner.Application.Database;
+using Planner.Application.Models;
 
 namespace Planner.Application.Services;
 
@@ -12,4 +13,29 @@ public class ContractorsRepository
         _factory = factory;
     }
 
+    public async Task<List<Contractor>> List()
+    {
+        await using var context = await _factory.CreateDbContextAsync();
+        return await context.Contractors.OrderBy(x=>x.Name).ToListAsync();
+    }
+
+    public async Task<Contractor> StoreOrUpdate(Contractor data)
+    {
+        await using var context = await _factory.CreateDbContextAsync();
+        if (data.Id == 0)
+        {
+	        var existed = await context.Contractors.FirstOrDefaultAsync(x => x.Name == data.Name);
+	        if (existed is not null) throw new InvalidOperationException("Trying store existed contractor");
+		}
+        context.Update(data);
+        await context.SaveChangesAsync();
+        return data;
+    }
+
+    public async Task Destroy(Contractor data)
+    {
+        await using var context = await _factory.CreateDbContextAsync();
+        context.Remove(data);
+        await context.SaveChangesAsync();
+    }
 }
---
+++ Application/Services/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Planner.Application.Models;
+
+namespace Planner.Application.Services;
+
+public static class Extensions
+{
+	public static void Copy(this Contractor target, Contractor source)
+	{
+		target.Id = source.Id;
+		target.Name = source.Name;
+		target.CreatedAt = source.CreatedAt;
+		target.UpdatedAt = source.UpdatedAt;
+	}
+}
