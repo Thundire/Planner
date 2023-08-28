45918aecb8cfbe095e0c125a2fa4b43ca894ec32
Tarxos <ftarxos@gmail.com>
2023-08-29 00:33:52 +0300

    change datamodels for Goal and Contractor, remove autoinclude navigation properties as it will not updated every time on accesing data

--- Application/Models/Contractor.cs
+++ Application/Models/Contractor.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Contracts;
 using Spark.Library.Database;
 
 namespace Planner.Application.Models;
@@ -5,4 +6,10 @@ namespace Planner.Application.Models;
 public class Contractor : BaseModel
 {
     public string Name { get; set; } = string.Empty;
+
+    public static Contractor Empty { get; } = new() { Name = "Empty" };
+
+	public override bool Equals(object? obj) => obj is Contractor contractor && Name == contractor.Name;
+	public override int GetHashCode() => HashCode.Combine(Name);
+	public override string ToString() => Name;
 }
--- Application/Models/Goal.cs
+++ Application/Models/Goal.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Spark.Library.Database;
 
 namespace Planner.Application.Models;
@@ -5,12 +6,18 @@ namespace Planner.Application.Models;
 public class Goal : BaseModel
 {
     public string Name { get; set; } = "NoName";
-    public virtual Contractor Contractor { get; set; } = new();
     public TimeSpan ElapsedTime { get; set; }
     public string Comment { get; set; } = string.Empty;
-    public List<GoalElapsedTimePart> ElapsedTimeParts { get; set; } = new ();
+    [NotMapped]
+    public bool ShowParts { get; set; }
+    [NotMapped]
+    public bool ShowCollapsedParts { get; set; }
 
-    public virtual User User { get; set; } = new();
+    public List<GoalElapsedTimePart> ElapsedTimeParts { get; set; } = new();
+	public int ContractorId { get; set; }
+    public Contractor? Contractor { get; set; } = new();
+	public int UserId { get; set; }
+    public User? User { get; set; } = new();
 
     public void CollapseElapsedTime(bool fixCollapsed = false)
     {
