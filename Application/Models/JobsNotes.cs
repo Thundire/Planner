83b7fa9a90caff3f8b5c8d481c23617860a85e7d
Tarxos <ftarxos@gmail.com>
2023-09-14 00:18:31 +0300

    change JobsHistory presentation

--- Application/Models/JobsNotes.cs
+++ Application/Models/JobsNotes.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Spark.Library.Database;
 
 namespace Planner.Application.Models;
@@ -7,4 +8,5 @@ public class JobsNotes : BaseModel
 	public DateTime Date { get; set; }
 	public User? User { get; set; }
 	public List<JobsNote> Notes { get; set; } = new();
+	[NotMapped] public bool IsDetailsShown { get; set; }
 }
